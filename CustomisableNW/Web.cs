0112a4ad62bdda16512fdfbfb660f64cdb3bed8f
Nikidomany <nikita.domanskiy1@gmail.com>
2024-01-18 00:01:28 +0300

    added function printing activations in textBox

--- CustomisableNW/Web.cs
+++ CustomisableNW/Web.cs
@@ -51,11 +51,9 @@ namespace CustomisableNW
 
             WeightsInitialize();
             NeuronsInitialize();
-            PrintRandomWeights();
+            PrintWeights();
+            ActivationsComputing();
         } 
-
-
-
 
         private void WeightsInitialize()
         {
@@ -63,7 +61,7 @@ namespace CustomisableNW
             for(int i = 0; i < hiddenLayersNumber+1; i++) // создание слоя весов
             {
                 weights.Add(new List<List<Weight>>());
-                for(int j = 0; j < (i < neuronsPerLayer.Count ? neuronsPerLayer[i] : 2); j++) // создание весов, идущих к одному нейрону
+                for(int j = 0; j < (i < neuronsPerLayer.Count ? neuronsPerLayer[i] : 1); j++) // создание весов, идущих к одному нейрону
                 {
                     weights[i].Add(new List<Weight>());
                     for(int k = 0; k < (i == 0 ? 2 : neuronsPerLayer[i-1]); k++) // создание конкретного веса
@@ -77,19 +75,18 @@ namespace CustomisableNW
         }
         private void NeuronsInitialize()
         {
-            for(int i = 0; i < hiddenLayersNumber + 1; i++)
+            for(int i = 0; i < hiddenLayersNumber; i++)
             {
                 neurons.Add(new List<Neuron>());    // adding a hidden layer
 
-                for (int j = 0; j < (i < hiddenLayersNumber ? neuronsPerLayer[i] : 1); j++)
+                for (int j = 0; j < neuronsPerLayer[i]; j++)
                     neurons[i].Add(new Neuron());   // adding a neuron to the hidden layer
             }
             neurons.Add(new List<Neuron> { new Neuron() });     // adding an output neuron
         }
 
 
-
-        void PrintRandomWeights()
+        private void PrintWeights()
         {
             string result = "";
 
@@ -98,13 +95,13 @@ namespace CustomisableNW
 
             for (int i = 0; i < weights.Count; i++) // веса между слоями
             {
-                result += $"\r\n   • {(i == 0 ? "input layer - 2" : (i == weights.Count - 1 ? $"{i+1} layer - output" : $"{i+1} layer - {i + 2}"))} layer:";
-                result += "\r\n       ";
+                result += $"\r\n   • {(i == 0 ? "input layer - 1" : (i == weights.Count - 1 ? $"{i} layer - output" : $"{i} layer - {i + 1}"))} layer:";
+                result += "\r\n           ";
                 for (int x = 0; x < weights[i].Count; x++) // X axis: N10  N11  N12...
                     result += $"  N{i+1}{x}  ";
                 for (int j = 0; j < (i == 0 ? 2 : weights[i-1].Count); j++) // веса ведущие к одному нейрону
                 {
-                    result += $"\r\n N{i}{j}";
+                    result += $"\r\n     N{i}{j}";
                     for (int k = 0; k < weights[i].Count; k++)
                     {
                         string sign = (weights[i][k][j].Value < 0 ? "" : " ");
@@ -115,14 +112,33 @@ namespace CustomisableNW
 
             form.TextBox += result;
         }
-       
         private void PrintActivations()
         {
-            string result;
-            
+            string result = "\r\n\r\nNeuron activation:";
 
+            int maxNeuronsNumber = 0;
+            for(int i = 0; i < neuronsPerLayer.Count; i++) // biggest layer computing
+                if(neurons[i].Count > maxNeuronsNumber)
+                    maxNeuronsNumber = neuronsPerLayer[i];
 
+            result += "\r\n          ";
+            for (int i = 0; i < hiddenLayersNumber + 2; i++)
+            {
+                string layerName = (i == 0 ? "Inp" : (i == hiddenLayersNumber + 1 ? " Out" : $"  {i}-H "));
+                result += $"  {layerName} ";
+            }
 
+            for (int i = 0; i < maxNeuronsNumber; i++) // i-th neuron in j-th layer
+            {
+                result += $"\r\n     N{i} ";
+                result += $"   {(i < 2 ? $"{TrainSet.Input(TrainSetNumber())[i]}" : " -")}   "; // input value or "  " (when i>1)
+                for (int j = 0; j < neurons.Count; j++) // j-th layer
+                {
+                    string activation = (i < neurons[j].Count) ? (Math.Round(neurons[j][i].Activation,2).ToString()) : "  -   ";
+                    result += $"   {activation}  ";
+                }
+            }
+            form.TextBox += result;
         }
 
 
@@ -145,26 +161,21 @@ namespace CustomisableNW
                     }
                     else if(i == hiddenLayersNumber) // for the output neuron layer
                     {
-                        for (int k = 0; k < neuronsPerLayer[i - 1]; i++)
+                        for (int k = 0; k < neuronsPerLayer[i - 1]; k++)
                             inputValue += neurons[i - 1][k].Activation * weights[i][j][k].Value;
                     }
 
                     neurons[i][j].Activation = ActivationFunction.Sigmoid(inputValue); // recording output neuron value
                 }
+            PrintActivations();
         }
 
 
         
-
-
-
-
-
-
         
         private int TrainSetNumber()
         {
-            return 10;
+            return 1;
         }
         
     }
@@ -210,7 +221,6 @@ namespace CustomisableNW
         }
 
     }
-
 
 
 
